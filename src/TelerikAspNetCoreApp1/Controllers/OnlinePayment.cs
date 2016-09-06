659162301a6d843dcf651adc341cdad0d64e80d7
Jonathan Tecson <jonathan.tecson@corearth.com>
2016-09-06 13:44:42 +1000

    adding of stripe

---
+++ src/TelerikAspNetCoreApp1/Controllers/OnlinePayment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TelerikAspNetCoreApp1.Controllers
+{
+    public class OnlinePayment : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CheckoutPayment()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult APIPayment()
+        {
+            return View();
+        }
+
+
+
+    }
+}
--- src/TelerikAspNetCoreApp1/Startup.cs
+++ src/TelerikAspNetCoreApp1/Startup.cs
@@ -15,6 +15,7 @@ namespace TelerikAspNetCoreApp1
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
+
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
