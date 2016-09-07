84d6998f59f4efb7c1156e355a560307a0d0afcf
Jonathan Tecson <jonathan.tecson@corearth.com>
2016-09-07 14:47:02 +1000

    initial stripe checkout payment

--- src/TelerikAspNetCoreApp1/Controllers/OnlinePayment.cs
+++ src/TelerikAspNetCoreApp1/Controllers/OnlinePayment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,14 +17,17 @@ namespace TelerikAspNetCoreApp1.Controllers
             return View();
         }
 
-
+        [HttpGet]
         public IActionResult CheckoutPayment()
         {
+
+            var stripeAPI = new StripePlanService();
+
+
             return View();
         }
 
-
-
+        
         public IActionResult APIPayment()
         {
             return View();
