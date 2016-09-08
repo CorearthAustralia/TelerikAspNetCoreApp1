b4ab1145b2977d25cea7fa6066c085c5a320c792
Jonathan Tecson <jonathan.tecson@corearth.com>
2016-09-08 11:49:57 +1000

    Striope changes2

--- src/TelerikAspNetCoreApp1/Controllers/OnlinePayment.cs
+++ src/TelerikAspNetCoreApp1/Controllers/OnlinePayment.cs
@@ -17,17 +17,46 @@ namespace TelerikAspNetCoreApp1.Controllers
             return View();
         }
 
-        [HttpGet]
         public IActionResult CheckoutPayment()
         {
 
             var stripeAPI = new StripePlanService();
 
+            
+
 
             return View();
         }
 
-        
+        [HttpPost]
+        public IActionResult CheckoutPayment(string StripeToken, string CustomerID, int ChargeAmount)
+        {
+
+            //var myCustomer = new StripeCustomerCreateOptions();
+            //myCustomer.Email = "jonathan.tecson@corearth.com";
+            //myCustomer.Description = "Jonathan Tecson";
+            //myCustomer.SourceToken = StripeToken;
+            ////myCustomer.TrialEnd = DateTime.UtcNow.AddMonths(1);    // when the customers trial ends (overrides the plan if applicable)
+            ////myCustomer.Quantity = 1;                               // optional, defaults to 1
+            //var customerService = new StripeCustomerService("sk_test_qFGjHTjY0gFwJsqkpskaiTLP");
+            //StripeCustomer stripeCustomer = customerService.Create(myCustomer);
+
+            var StripeCharge = new StripeChargeCreateOptions();
+            StripeCharge.Amount = ChargeAmount;
+            StripeCharge.Currency = "aud";
+            StripeCharge.Description = "Jupiter Subscription";
+            StripeCharge.SourceTokenOrExistingSourceId = StripeToken;
+            
+            StripeCharge.Capture = true;
+
+            var StripeChargeService = new StripeChargeService("sk_test_qFGjHTjY0gFwJsqkpskaiTLP");
+            StripeCharge StripeChargeTrans = StripeChargeService.Create(StripeCharge);
+
+            return View();
+        }
+
+
+
         public IActionResult APIPayment()
         {
             return View();
